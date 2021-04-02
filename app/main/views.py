a208793d78ff8fb81eab46b3d2af681539703417
doujingyue <jingyue.dou@ucdconnect.ie>
2021-04-02 19:23:00 +0800

    second_push 04_02

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import request, redirect, render_template, url_for
+
+from app.main import main
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    global role_id
+    if request.method == 'POST':
+        role_id = request.args.get('identification')
+        return redirect(url_for('main.login'))  
+
+    return render_template("index.html")
