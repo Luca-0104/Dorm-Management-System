5322a630f9cfa6f4e05fc7d1edc60100cb7970ec
gilbertyou <1378149717@qq.com>
2021-04-06 09:00:08 +0800

    test2

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
 
 
--- app/static/js/skip.js
+++ app/static/js/skip.js
@@ -1,15 +1,15 @@
 // JavaScript Document
 $('#student').click(function () {
-	alert('/login');
+	// alert('/login');
 	location.href = '/login?identification=student'
 });
 
 $('#dormintoryAdmin').click(function () {
-	alert('/login');
+	// alert('/login');
 	location.href = '/login?identification=dormitoryAdmin'
 });
 
 $('#systemAdmin').click(function () {
-	alert('/login');
+	// alert('/login');
 	location.href = '/login?identification=systemAdmin'
 });
