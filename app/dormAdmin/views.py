8df84a54b71a2a022e0f7a6f804ef245dcd28cce
doujingyue <jingyue.dou@ucdconnect.ie>
2021-04-15 14:09:45 +0800

    Third_push 04_14

--- app/dormAdmin/views.py
+++ app/dormAdmin/views.py
@@ -1,7 +1,7 @@
 @dormAdmin.route('/delete', endpoint='delete')
 def delete():
     id = request.args.get('id')
-    user = User.query.get(id)
-    user.is_deleted = False
+    student = User.query.get(id)
+    student.is_deleted = True
     db.session.commit()
     return redirect(url_for('dormAdmin.home'))
