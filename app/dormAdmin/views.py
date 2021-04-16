561dc27bf3d061b017aa04b1f94400974dbc5ed7
Luca <zhe.liu1@ucdconnect.ie>
2021-04-16 11:17:42 +0800

    search_stu function checked!

--- app/dormAdmin/views.py
+++ app/dormAdmin/views.py
@@ -10,44 +10,48 @@ from ..models import Student, Guest
 # students CRUD ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-@dormAdmin.route('/search_stu', methods=['GET', 'POST'])  # 路由名待完善核对
+@dormAdmin.route('/search_stu', methods=['GET', 'POST'])
 def search_stu():
     """
     fuzzy querying was used
     :return: stu_list ab list of Student objects that meet the filter requirement
     """
-    return "yes"
-    key_word = request.args.get('search')  # 待完善核对
-    tag = request.form.get('tag')  # 待完善核对
+    key_word = request.args.get('content')
+    tag = request.args.get('tag')
 
+    print('tag: ' + tag)
+    print('key_word:' + key_word)
+
+    stu_list = []
     if tag == 'all':
         stu_list = Student.query.filter(and_(or_(Student.stu_name.contains(key_word),
-                                                 Student.stu_number == key_word,
+                                                 Student.stu_number.contains(key_word),
                                                  Student.phone.contains(key_word),
                                                  Student.college.contains(key_word),
-                                                 str(Student.room_number) == key_word,
-                                                 str(Student.enroll_date).contains(key_word)
-                                                 )), not Student.is_deleted).all()
+                                                 Student.room_number.contains(key_word),
+                                                 Student.enroll_date.contains(key_word)
+                                                 )), Student.is_deleted == False).all()
 
     elif tag == 'stu_name':
-        stu_list = Student.query.filter(and_(Student.stu_name.contains(key_word), not Student.is_deleted)).all()
+        stu_list = Student.query.filter(and_(Student.stu_name.contains(key_word), Student.is_deleted == False)).all()
 
     elif tag == 'stu_number':
-        stu_list = Student.query.filter(and_(Student.stu_number.contains(key_word), not Student.is_deleted)).all()
+        stu_list = Student.query.filter(and_(Student.stu_number.contains(key_word), Student.is_deleted == False)).all()
 
     elif tag == 'phone':
-        stu_list = Student.query.filter(and_(Student.phone.contains(key_word), not Student.is_deleted)).all()
+        stu_list = Student.query.filter(and_(Student.phone.contains(key_word), Student.is_deleted == False)).all()
 
     elif tag == 'college':
-        stu_list = Student.query.filter(and_(Student.college.contains(key_word), not Student.is_deleted)).all()
+        stu_list = Student.query.filter(and_(Student.college.contains(key_word), Student.is_deleted == False)).all()
 
     elif tag == 'room_number':
-        stu_list = Student.query.filter(and_(str(Student.room_number).contains(key_word), not Student.is_deleted)).all()
+        stu_list = Student.query.filter(and_(Student.room_number.contains(key_word), Student.is_deleted == False)).all()
 
     elif tag == 'enroll_date':
-        stu_list = Student.query.filter(and_(str(Student.enroll_date).contains(key_word), not Student.is_deleted)).all()
+        stu_list = Student.query.filter(and_(Student.enroll_date.contains(key_word), Student.is_deleted == False)).all()
 
-    return render_template('../templates/samples/testindex.html', stu_list=stu_list)  # 待完善核对
+    print(stu_list)
+    return render_template('samples/testindex.html', students=stu_list)  # 待完善核对
 
 
 @dormAdmin.route('/delete_stu', endpoint='delete')
@@ -56,7 +60,7 @@ def delete_stu():
     student = Student.query.get(id)
     student.is_deleted = True
     db.session.commit()
-    return redirect(url_for('auth.home'))  # 待完善核对
+    return redirect(url_for('main.home_dorm_admin'))  # 待完善核对
 
 
 @dormAdmin.route('/add_stu', methods=['GET', 'POST'])
@@ -73,7 +77,7 @@ def add_stu():
         db.session.add(new_student)
         db.session.commit()
 
-    return render_template('auth.home')  # 待完善核对
+    return render_template('main.home_dorm_admin')  # 待完善核对
 
 
 # guests CRUD ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -82,7 +86,7 @@ def add_stu():
 @dormAdmin.route('/search_gue', methods=['GET', 'POST'])  # 路由名待完善核对
 def search_gue():
     key_word = request.args.get('key_word')  # 待完善核对
-    tag = request.form.get('tag')  # 待完善核对
+    tag = request.args.get('tag')  # 待完善核对
 
     if tag == 'all':
         stu = Student.query.filter_by(stu_number=key_word).first()
@@ -90,47 +94,47 @@ def search_gue():
             gue_list = Guest.query.filter(and_(or_(Guest.gue_name.contains(key_word),
                                                    Guest.phone.contains(key_word),
                                                    Guest.stu_id == stu.id,
-                                                   str(Guest.room_number) == key_word,
-                                                   str(Guest.building_id) == key_word,
-                                                   str(Guest.arrive_time).contains(key_word),
-                                                   str(Guest.leave_time).contains(key_word),
-                                                   )), not Guest.is_deleted).all()
+                                                   Guest.room_number == key_word,
+                                                   Guest.building_id == key_word,
+                                                   Guest.arrive_time.contains(key_word),
+                                                   Guest.leave_time.contains(key_word),
+                                                   )), Guest.is_deleted == False).all()
         else:
             gue_list = Guest.query.filter(and_(or_(Guest.gue_name.contains(key_word),
                                                    Guest.phone.contains(key_word),
-                                                   str(Guest.room_number) == key_word,
-                                                   str(Guest.building_id) == key_word,
-                                                   str(Guest.arrive_time).contains(key_word),
-                                                   str(Guest.leave_time).contains(key_word),
-                                                   )), not Guest.is_deleted).all()
+                                                   Guest.room_number == key_word,
+                                                   Guest.building_id == key_word,
+                                                   Guest.arrive_time.contains(key_word),
+                                                   Guest.leave_time.contains(key_word),
+                                                   )), Guest.is_deleted == False).all()
 
     elif tag == 'gue_name':
-        gue_list = Guest.query.filter(and_(Guest.stu_name.contains(key_word), not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.stu_name.contains(key_word), Guest.is_deleted == False)).all()
 
     elif tag == 'stu_number':
         ref_stu_list = Student.query.filter(Student.stu_number.contains(key_word)).all()
         gue_list = []
         for stu in ref_stu_list:
-            gue = Guest.query.filter(and_(Guest.stu_id == stu.id, not Guest.is_deleted)).first()
+            gue = Guest.query.filter(and_(Guest.stu_id == stu.id, Guest.is_deleted == False)).first()
             if gue:
                 gue_list.append(gue)
 
     elif tag == 'phone':
-        gue_list = Guest.query.filter(and_(Guest.phone.contains(key_word), not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.phone.contains(key_word), Guest.is_deleted == False)).all()
 
     elif tag == 'relation':
-        gue_list = Guest.query.filter(and_(Guest.relation.contains(key_word), not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.relation.contains(key_word), Guest.is_deleted == False)).all()
 
     elif tag == 'building_id':
-        gue_list = Guest.query.filter(and_(str(Guest.building_id) == key_word, not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.building_id == key_word, Guest.is_deleted == False)).all()
 
     elif tag == 'room_number':
-        gue_list = Guest.query.filter(and_(str(Guest.room_number) == key_word, not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.room_number == key_word, Guest.is_deleted == False)).all()
 
     elif tag == 'arrive_time':
-        gue_list = Guest.query.filter(and_(str(Guest.arrive_time).contains(key_word), not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.arrive_time.contains(key_word), Guest.is_deleted == False)).all()
 
     elif tag == 'leave_time':
-        gue_list = Guest.query.filter(and_(str(Guest.leave_time).contains(key_word), not Guest.is_deleted)).all()
+        gue_list = Guest.query.filter(and_(Guest.leave_time.contains(key_word), Guest.is_deleted == False)).all()
 
     return render_template('.html', gue_list=gue_list)  # 待完善核对
