44a77a48d5876b73858b5c36531c02267463486a
zhubolun <1018542633@qq.com>
2021-05-03 18:59:10 +0800

    send message-2

--- app/student/views.py
+++ app/student/views.py
@@ -1,58 +1,27 @@
-from datetime import datetime
+from flask import request, render_template
+from flask import Blueprint
+from .. import db
+from app.models import Message, Student
 
-from flask import request, redirect, render_template, url_for, flash
-from flask_login import UserMixin, current_user
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length
+message_bp = Blueprint('message', __name__)
 
-from .. import db, student
-from app.main.forms import MessageForm
-from app.models import Message
-
-
-# 新增数据库模型
-class Message(db.Model):
-    __tablename__ = 'messages'
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('stu_id'))
-    recipient_id = db.Column(db.Integer, db.ForeignKey('dorm_id'))
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Message {}>'.format(self.body)
-
-
-# user 模型
-class User(UserMixin, db.Model):
-    messages_sent = db.relationship('Message',
-                                    foreign_keys='Message.sender_id',
-                                    backref='sender', lazy='dynamic')
-    messages_received = db.relationship('Message',
-                                        foreign_keys='Message.recipient_id',
-                                        backref='recipient', lazy='dynamic')
-
-
-# 私信表单
-
-class MessageForm(FlaskForm):
-    message = TextAreaField('Message', validators=[
-        DataRequired(), Length(min=0, max=140)])
-    submit = SubmitField('Submit')
-
-
-# 私信的视图函数
-@student.route('/send_message/<recipient>', methods=['GET', 'POST'])
-def send_message(recipient):
-    user = User.query.filter_by(username=recipient).first_or_404()
-    form = MessageForm()
-    if form.validate_on_submit():
-        msg = Message(author=current_user, recipient=user,
-                      body=form.message.data)
-        db.session.add(msg)
+@message_bp.route('/add', method=['GET', 'POST'])
+def add_message():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        uid = request.form.get('uid')
+        message = Message()
+        message.title = title
+        message.content = content
+        message.stu_id = uid
+        db.session.add(message)
         db.session.commit()
-        flash('Your message has been sent.')
-        return redirect(url_for('main.user', username=recipient))
-    return render_template('send_message.html', title='Send Message',
-                           form=form, recipient=recipient)
+        return 'submit successfully！'
+    else:
+        students = Student.query.filter(Student.is_deleted == False).all()
+        return render_template('message/add_message.html', students=students)
+
+
+
+
