6cfb6afe673e0b1bdff46af2e1763d0ccec3f383
Luca <zhe.liu1@ucdconnect.ie>
2021-05-19 10:54:00 +0800

    backend-frontend-merging

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -11,3 +11,9 @@ $('#search2').click(function(){
 	location.href="/dormAdmin/search_gue?content="+content+"&tag="+tag;
 });
 
+$('#search3').click(function(){
+	let content = $("input[name='search_da']").val();
+	var tag = document.getElementById("inputGroupSelect02").value;
+	location.href="/dormAdmin/search_gue?content="+content+"&tag="+tag;
+});
+
