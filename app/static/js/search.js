98ea9798b6e95bd43faa2dc2121e0d5dd67732b3
gilbertyou <1378149717@qq.com>
2021-06-09 20:05:34 +0800

    add pics

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -2,13 +2,13 @@
 $('#search1').click(function(){
 	let content = $("input[name='search']").val();
 	var tag = document.getElementById("inputGroupSelect01").value;
-	location.href="/sysAdmin/search_stu?content="+content+"&tag="+tag;
+	location.href="/dormAdmin/search_stu?content="+content+"&tag="+tag;
 });
 
 $('#search2').click(function(){
 	let content = $("input[name='search_gue']").val();
 	var tag = document.getElementById("inputGroupSelect02").value;
-	location.href="/sysAdmin/search_gue?content="+content+"&tag="+tag;
+	location.href="/dormAdmin/search_gue?content="+content+"&tag="+tag;
 });
 
 $('#search3').click(function(){
